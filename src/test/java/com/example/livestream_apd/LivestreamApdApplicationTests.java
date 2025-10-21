94febb4820e8ddc02ea71e27db1bab207bc85b57
Nguyễn Duy Hưng <hungnguyen@MacBook-Pro-cua-Nguyen-2.local>
2025-10-21 09:16:51 +0700

    Day 8: Add unit tests for services and controllers

---
+++ src/test/java/com/example/livestream_apd/LivestreamApdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livestream_apd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestreamApdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
